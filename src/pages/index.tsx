0c5473f631d13c0adf042b96bd24bebd0ca67816
Rafinhaa <rafinhaa@me.com>
2022-09-21 11:51:40 -0300

    Estrutura visual - Estrutura da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,28 @@
+import Image from "next/future/image";
 import React from "react";
-import { styled } from "../styles/styles";
+import { HomeContainer, Product } from "../styles/pages/home";
 
-const Button = styled("button", {
-  backgroundColor: "$green300",
-  borderRadius: 4,
-  border: 0,
-  padding: "4px 8px",
-  span: {
-    fontWeight: "bold",
-  },
-  "&:hover": {
-    filter: "brightness(0.8)",
-  },
-});
+import camiseta1 from "../assets/camisetas/1.png";
+import camiseta2 from "../assets/camisetas/2.png";
+import camiseta3 from "../assets/camisetas/3.png";
 
 export default function Home() {
   return (
-    <Button>
-      <span>Hello </span>world
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} alt="" width={520} height={480} />
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta2} alt="" width={520} height={480} />
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
