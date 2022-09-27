728e8f454c28b279f4e9e9795bd238b4f7289260
Rafinhaa <rafinhaa@me.com>
2022-09-27 10:55:24 -0300

    Produto & Checkout - Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,27 @@
 import { useRouter } from "next/router";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "../../styles/pages/product";
 
 export default function Product() {
   const { query } = useRouter();
 
-  return <h1>{JSON.stringify(query)}</h1>;
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,00</span>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta autem
+          doloremque tempora alias ut quidem architecto natus rerum, similique
+          dolore dicta, earum in, ducimus excepturi quo! Perferendis quam hic
+          alias?
+        </p>
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
