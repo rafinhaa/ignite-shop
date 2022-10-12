e1343f36ed4528b1f503a7c9798c2b3172c483b5
Rafinhaa <rafinhaa@me.com>
2022-10-12 09:48:37 -0300

    feat: Cria CartContext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,14 +5,17 @@ import { AppProps } from "next/app";
 import { globalStyles } from "../styles/global";
 import { Container } from "../styles/pages/app";
 import { Header } from "../components/Header";
+import { CartContextProvider } from "../context/CartContext";
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Container>
-      <Header />
-      <Component {...pageProps} />
-    </Container>
+    <CartContextProvider>
+      <Container>
+        <Header />
+        <Component {...pageProps} />
+      </Container>
+    </CartContextProvider>
   );
 }
